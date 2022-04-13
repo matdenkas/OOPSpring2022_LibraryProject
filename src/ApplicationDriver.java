b89f85b1417deb1e904f5fc620c8bdd3d8803a88
K <B>
2022-04-12 20:19:45 -0500

    TODOS

--- src/ApplicationDriver.java
+++ src/ApplicationDriver.java
@@ -1,3 +1,19 @@
+//TODO::Create data handler class for all Media Entries
+    //TODO::search method
+
+//TODO::Create data handler class for all LibraryActors
+    //TODO::search method
+
+//TODO::Build log in page
+
+//TODO::Build normal page
+    //TODO::Window to see MediaEntries and check out
+    //TODO::Window to add mediaEntries/remove (if employee)
+    //TODO::Window to manage clients
+
+//TODO::Build the application Driver
+    //TODO::hard coded data
+
 public class ApplicationDriver {
 
     public static void main(String[] args) {
