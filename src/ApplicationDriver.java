9dad81d7c582f2a8f3c67426abe07ca2f23e9de1
charmii20 <charmidpatel20@gmail.com>
2022-04-12 19:35:24 -0500

    Added the line to say hello

--- src/ApplicationDriver.java
+++ src/ApplicationDriver.java
@@ -2,5 +2,6 @@ public class ApplicationDriver {
 
     public static void main(String[] args) {
         System.out.println("Hello, Charmi and Jose!");
+        System.out.println("Hello, Jose and Matthew!");
     }
 }
